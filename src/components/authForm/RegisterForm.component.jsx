f95d66ab0cdd2d49a3c9894c05fd607d847fb557
Karthik371 <70939080+Karthik371@users.noreply.github.com>
2021-09-15 16:19:10 +0530

    new chs

--- src/components/authForm/RegisterForm.component.jsx
+++ src/components/authForm/RegisterForm.component.jsx
@@ -135,7 +135,7 @@ const RegisterForm = ({ setAlert, registerUser }) => {
       {loginErrors ? (
         <div className="login-error">
           <p>Please clear your Cached images and files</p>
-          <p>{"Settings -> Privacy&Security -> clear browsing datas"}</p>
+          <p>{"Settings -> Privacy&Security -> clear browsing data"}</p>
           <p>{"(if you closed the google popup please Ignore this message)"}</p>
         </div>
       ) : (
